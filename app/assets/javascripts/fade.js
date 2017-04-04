0181b3279453d59f01b66f6213dbd1e55770574c
Lawrence Temple <lawrence.temple@mail.utoronto.ca>
2017-04-04 14:56:20 -0400

    More contiguous style. Site is probably ready to be populated with all real data

--- app/assets/javascripts/fade.js
+++ app/assets/javascripts/fade.js
@@ -1,4 +1,5 @@
-//From http://stackoverflow.com/questions/17754852/how-to-fade-in-a-div-on-hover-mouseover-using-jquery
+//Help from http://stackoverflow.com/questions/17754852/how-to-fade-in-a-div-on-hover-mouseover-using-jquery
+
 $(function() {
   $('#accordion').hover(function() { 
     $('#fade').fadeOut(); 
